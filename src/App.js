f83aa200a84912eb30175f9bddd3d16fde28906f
Joe Frost <cex@cexs-mac-mini.home>
2021-06-27 19:17:31 +0100

    change to beta redux toolkit, add dog api dummy info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, {useState} from 'react';
+import { createApi } from '@reduxjs/toolkit/dist/query';
 import './App.css';
 import { Counter } from './features/counter/Counter'
 
+
 function App() {
+
+  const [numDogs, setNumDogs] = useState(10)
+
+  const data = [{id:"dog-34345",name:"Terrier",image: {url:""}},{id:"dog-34346",name:"Alsation",image: {url:""}}]
+
   const myObject={
     users:[
       {
@@ -27,6 +35,25 @@ function App() {
       </ul>
       </div>
       <span>Version: {appVersion}</span>
+      <hr />
+      <div><p>Dogs to fetch:</p><select value={numDogs} onChange={(e) => setNumDogs(Number(e.target.value))}>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+      </select></div>
+      <div>
+        <p>Number of dogs fetched: {data.length}</p>
+        <table>
+          <thead><tr><th>Name</th><th>Picture</th></tr></thead>
+          <tbody>
+            {data.map(breed=>(<tr key={breed.id}>
+                <td>{breed.name}</td>
+                <td><img alt={breed.name} src={breed.image.url} height="250" /></td>
+              </tr>))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/features/dogs/dogsApiSlice.js
@@ -0,0 +1,3 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const DOGS_API_KEY = 'd4cea253-e6c7-4ba3-bb6c-6ad31dfa405a'
